0ae52afacd1bd2634f7ab22a3ecb8a67cf0cea41
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-03 15:38:08 +0900

    開発の基盤作成完了

---
+++ vue_app/src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <router-link to="/page1">Go to Page 1</router-link>
+        <br>
+        <router-link to="/page2">Go to Page 2</router-link>
+        <br>
+        <router-link to="/page3">Go to Page 3</router-link>
+    </div>
+</template>
