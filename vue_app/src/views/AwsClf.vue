18fd628ca6e3d5e537553519994fd8d89fd34957
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-04 15:26:25 +0900

    CLF画面のテストデータ表示

--- vue_app/src/views/AwsClf.vue
+++ vue_app/src/views/AwsClf.vue
@@ -1,6 +1,47 @@
 <template>
     <div>
         <h1>クラウドプラクティショナー</h1>
+
+
+        <!-- 各カテゴリをループ -->
+        <div v-for="(words, category) in data" :key="category">
+
+            <!-- 各カテゴリの名前を表示 -->
+            <h2>{{ category }}</h2>
+
+            <!-- カテゴリ内のアイテムをリスト表示 -->
+            <ul>
+                <li v-for="word in words" :key="word">{{ word }}</li>
+            </ul>
+        </div>
+
+
         <router-link to="/">ホーム画面に戻る</router-link>
     </div>
 </template>
+
+<script>
+export default {
+    // データ格納オブジェクト
+    data() {
+        return {
+            data: {}
+        };
+    },
+
+    // マウント時にデータ取得
+    mounted() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            this.data = {
+                // テストデータ(カテゴリ : アイテムの配列)
+                "クラウド": ["クラウド", "コロケーション", "ホスティング"],
+                "コンピューティング": ["Amazon EC2", "Amazon マシンイメージ", "インスタンスタイプ"],
+                "ストレージ": ["ブロックストレージ", "ファイルストレージ", "オブジェクトストレージ"]
+            };
+        }
+    }
+};
+</script>
