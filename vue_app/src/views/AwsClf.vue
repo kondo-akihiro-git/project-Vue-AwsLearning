59f933e2ae04ac338af5181960ac1c61db14fd42
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-04 18:04:10 +0900

    テストデータの追加とコンポーネント修正

--- vue_app/src/views/AwsClf.vue
+++ vue_app/src/views/AwsClf.vue
@@ -1,31 +1,18 @@
-<template>
-    <div>
-        <h1>クラウドプラクティショナー</h1>
-
-
-        <!-- 各カテゴリをループ -->
-        <div v-for="(words, category) in data" :key="category">
-
-            <!-- 各カテゴリの名前を表示 -->
-            <h2>{{ category }}</h2>
-
-            <!-- カテゴリ内のアイテムをリスト表示 -->
-            <ul>
-                <li v-for="word in words" :key="word">{{ word }}</li>
-            </ul>
-        </div>
-
-
-        <router-link to="/">ホーム画面に戻る</router-link>
-    </div>
-</template>
-
 <script>
+import WordDetail from '../components/WordDetail.vue';
+import CategoryList from '../components/CategoryList.vue';
+
 export default {
+    // コンポーネントを登録
+    components: {
+        WordDetail,
+        CategoryList
+    },
     // データ格納オブジェクト
     data() {
         return {
-            data: {}
+            wordsData: {},
+            selectedWord: null,
         };
     },
 
@@ -34,14 +21,45 @@ export default {
         this.fetchData();
     },
     methods: {
+        // データ取得メソッド
         fetchData() {
-            this.data = {
-                // テストデータ(カテゴリ : アイテムの配列)
+            this.wordsData = {
+                // テストデータ(カテゴリ : ワードの配列)
                 "クラウド": ["クラウド", "コロケーション", "ホスティング"],
                 "コンピューティング": ["Amazon EC2", "Amazon マシンイメージ", "インスタンスタイプ"],
                 "ストレージ": ["ブロックストレージ", "ファイルストレージ", "オブジェクトストレージ"]
             };
+        },
+
+        // ワード詳細を表示するメソッド
+        showWordDetail(word) {
+            this.selectedWord = word;
+        },
+
+        // 詳細を閉じるメソッド
+        closeWordDetail() {
+            this.selectedWord = null;
         }
     }
 };
 </script>
+
+<template>
+    <!-- ///////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // クラウドプラクティショナー取得に関する学習カテゴリーとワードの表示
+    //
+    //////////////////////////////////////////////////////////////////////////////////////////////// -->
+    <div>
+        <h1>クラウドプラクティショナー</h1>
+
+        <!-- カテゴリとワード一覧を表示 -->
+        <CategoryList :wordsData="wordsData" @showWordDetailEvent="showWordDetail" />
+
+        <!-- ワード詳細を表示 -->
+        <WordDetail v-if="selectedWord" :word="selectedWord" @closeWordDetailEvent="closeWordDetail"/>
+
+
+        <router-link to="/">ホーム画面に戻る</router-link>
+    </div>
+</template>
