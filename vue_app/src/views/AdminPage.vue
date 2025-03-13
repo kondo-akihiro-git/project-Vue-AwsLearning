06a3ceac099115e267a6c0a0b7a736a1af04da37
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-13 15:30:47 +0900

    管理者画面でのNotionへのアクセス追加

--- vue_app/src/views/AdminPage.vue
+++ vue_app/src/views/AdminPage.vue
@@ -2,17 +2,18 @@
 import AnnouncementForm from '@/components/AnnouncementForm.vue';
 
 export default {
-    // コンポーネントを登録
     components: {
         AnnouncementForm,
     },
     data() {
         return {
-            showForm: false
+            showForm: false,
+            notionWordsTable: process.env.VUE_APP_NOTION_WORD_TABLE_URL,
+            notionRequestWordsTable: process.env.VUE_APP_NOTION_REQUEST_TABLE_URL,
+            notionAnnouncementsTable: process.env.VUE_APP_NOTION_ANNOUNCEMENT_TABLE_URL,
         };
     },
     methods: {
-        // 周知フォームの表示切り替え
         toggleForm() {
             this.showForm = !this.showForm;
         }
@@ -25,7 +26,6 @@ export default {
         <section class="section">
             <h2 class="title is-3">管理画面</h2>
 
-            <!-- 周知フォームの表示の切り替えボタン -->
             <div class="field is-grouped is-grouped-centered">
                 <div class="control">
                     <button @click="toggleForm" class="button is-primary is-medium">
@@ -34,7 +34,12 @@ export default {
                 </div>
             </div>
 
-            <!-- 周知フォーム表示 -->
+            <div class="content">
+                <p><a :href="notionWordsTable" target="_blank" class="button is-link">ワード一覧テーブル</a></p>
+                <p><a :href="notionRequestWordsTable" target="_blank" class="button is-link">新規ワードの追加申請テーブル</a></p>
+                <p><a :href="notionAnnouncementsTable" target="_blank" class="button is-link">お知らせテーブル</a></p>
+            </div>
+
             <div v-if="showForm">
                 <AnnouncementForm @formSubmitted="showForm = false" @closeForm="showForm = false" />
             </div>
