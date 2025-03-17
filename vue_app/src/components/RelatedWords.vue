8bab256d9bd0ac7993e38a2da8965f65d836d2c8
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-17 15:59:13 +0900

    関連ワードのtypeIdによるデザイン対応

--- vue_app/src/components/RelatedWords.vue
+++ vue_app/src/components/RelatedWords.vue
@@ -6,6 +6,22 @@ export default {
     methods: {
         selectWord(word) {
             this.$emit('showWordDetailEvent', word);
+        },
+        getTypeClass(typeName) {
+            switch (typeName) {
+                case "service":
+                    return "border-aws-service";
+                case "method":
+                    return "border-tech-method";
+                case "technical term":
+                    return "border-tech-term";
+                case "attitude":
+                    return "border-aws-attitude";
+                case "aws term":
+                    return "border-aws-term";
+                default:
+                    return "border-default-type";
+            }
         }
     }
 };
@@ -16,7 +32,10 @@ export default {
         <h3 class="title is-4 m-2 mb-4">関連度の高いワード</h3>
         <div v-if="relatedWords.length > 0" class="columns is-multiline is-mobile is-gapless">
             <div v-for="word in relatedWords" :key="word.wordId" class="column is-narrow">
-                <button class="button is-info mx-1 my-1">
+                <button 
+                    class="button mx-1 my-1" 
+                    :class="getTypeClass(word.typeName)" 
+                    @click="selectWord(word)">
                     {{ word.wordName }}
                 </button>
             </div>
@@ -24,3 +43,30 @@ export default {
         <p class="m-2" v-else>関連ワードはありません</p>
     </div>
 </template>
+
+<style scoped>
+/* Typeに応じたボーダー色の設定 */
+.border-aws-service {
+    border-color: #ff9900 !important;
+}
+
+.border-tech-method {
+    border-color: #0073bb !important;
+}
+
+.border-tech-term {
+    border-color: #333333 !important;
+}
+
+.border-aws-attitude {
+    border-color: #2d7600 !important;
+}
+
+.border-aws-term {
+    border-color: #ffe600 !important;
+}
+
+.border-default-type {
+    border-color: #e0e0e0 !important;
+}
+</style>
