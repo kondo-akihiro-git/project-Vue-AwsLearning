4cf788a9c95354d90dff037dc2493674d084a4ee
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-18 13:58:44 +0900

    管理画面とお知らせ画面のレイアウト修正

--- vue_app/src/components/AnnouncementForm.vue
+++ vue_app/src/components/AnnouncementForm.vue
@@ -30,33 +30,31 @@ export default {
 
 <template>
     <div class="box">
-        <h2 class="title is-3">周知事項の登録</h2>
+        <h2 class="title is-3">お知らせの送信</h2>
         <form @submit.prevent="submit">
             <div class="field">
-                <label class="label">周知タイトル</label>
+                <label class="label">お知らせタイトル</label>
                 <div class="control">
                     <input v-model="title" class="input is-medium" type="text" placeholder="タイトルを入力" required />
                 </div>
             </div>
             
             <div class="field">
-                <label class="label">周知内容</label>
+                <label class="label">お知らせ内容</label>
                 <div class="control">
                     <textarea v-model="content" class="textarea is-medium" placeholder="内容を入力" required></textarea>
                 </div>
             </div>
 
-            <div class="field is-grouped is-grouped-centered">
+            <!-- ボタンを横並び、両端寄せに変更 -->
+            <div class="field is-grouped is-grouped-right is-flex is-justify-content-space-between">
                 <div class="control">
-                    <button type="submit" class="button is-primary is-medium">登録</button>
+                    <button type="submit" class="button is-medium">送信</button>
+                </div>
+                <div class="control">
+                    <button @click="$emit('closeForm')" class="button is-light is-medium">管理画面に戻る</button>
                 </div>
             </div>
         </form>
-
-        <div class="field is-grouped is-grouped-centered">
-            <div class="control">
-                <button @click="$emit('closeForm')" class="button is-light is-medium">戻る</button>
-            </div>
-        </div>
     </div>
 </template>
