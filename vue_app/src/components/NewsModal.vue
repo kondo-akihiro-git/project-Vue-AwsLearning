5e804336b3956d2455acececa7c5f997568b0762
kondo-akihiro-git <2000akisan@gmail.com>
2025-03-26 00:34:57 +0900

    モーダルのコンポーネント化

---
+++ vue_app/src/components/NewsModal.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="modal" :class="{ 'is-active': isActive }">
+        <div class="modal-background" @click="closeModal"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    {{ selectedAnnouncement ? selectedAnnouncement.titleName : '' }}
+                </p>
+                <button class="delete" aria-label="close" @click="closeModal"></button>
+            </header>
+            <section class="modal-card-body">
+                <p v-if="selectedAnnouncement">
+                    {{ selectedAnnouncement.content }}
+                </p>
+            </section>
+            <footer class="modal-card-foot is-justify-content-flex-end">
+                <button class="button is-light" @click="closeModal">閉じる</button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        isActive: {
+            type: Boolean,
+            required: true
+        },
+        selectedAnnouncement: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    }
+};
+</script>
+
+<style scoped>
+/* モーダルの最大幅設定 */
+.modal-card {
+    max-width: 800px;
+    width: 90%;
+}
+
+/* モーダルのコンテンツ中央配置 */
+.modal-card-body {
+    overflow-y: auto;
+    max-height: 60vh;
+    min-height: 25vh;
+}
+
+.modal-card-foot {
+    max-height: 10vh;
+}
+</style>
--- vue_app/src/views/NewsPage.vue
+++ vue_app/src/views/NewsPage.vue
@@ -2,11 +2,13 @@
 import { fetchAnnouncements } from '@/utils/service';
 import LogoHeader from '@/components/LogoHeader.vue';
 import DotLoading from '@/components/DotLoading.vue';
+import NewsModal from '@/components/NewsModal.vue';
 
 export default {
     components: {
         LogoHeader,
-        DotLoading
+        DotLoading,
+        NewsModal
     },
     data() {
         return {
@@ -100,25 +102,11 @@ export default {
         </div>
 
         <!-- モーダル -->
-        <div class="modal" :class="{ 'is-active': isModalActive }">
-            <div class="modal-background" @click="closeModal"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        {{ selectedAnnouncement ? selectedAnnouncement.titleName : '' }}
-                    </p>
-                    <button class="delete" aria-label="close" @click="closeModal"></button>
-                </header>
-                <section class="modal-card-body">
-                    <p v-if="selectedAnnouncement">
-                        {{ selectedAnnouncement.content }}
-                    </p>
-                </section>
-                <footer class="modal-card-foot is-justify-content-flex-end">
-                    <button class="button is-light" @click="closeModal">閉じる</button>
-                </footer>
-            </div>
-        </div>
+        <NewsModal
+            :isActive="isModalActive"
+            :selectedAnnouncement="selectedAnnouncement"
+            @close="closeModal"
+        />
     </div>
 
 </template>
@@ -132,20 +120,4 @@ export default {
     /* スクロール可能にする */
 }
 
-/* モーダルの最大幅設定 */
-.modal-card {
-    max-width: 800px;
-    width: 90%;
-}
-
-/* モーダルのコンテンツ中央配置 */
-.modal-card-body {
-    overflow-y: auto;
-    max-height: 60vh;
-    min-height: 25vh;
-}
-
-.modal-card-foot {
-    max-height: 10vh;
-}
 </style>
